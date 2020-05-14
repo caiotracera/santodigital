00cb74db3f304ae05e41cec5e433f6309fb5ca8c
Caio Tracera <ctrcra@gmail.com>
2020-05-14 18:08:13 -0300

    update phrases

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const phrases = ['Lorem ipsum dolor sit recusandae rem saepe aliquam.', ' Quidem temporibus iusto harum aut itaque incidunt.', 'Libero ipsa sapiente porro impedit iusto harum aut recusandae rem?', 'Repellat quae, sed non saepe aliquam nisi voluptate recusandae rem reiciendis iusto ducimus repudiandae facere.']
+const phrases = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui distinctio quas facere ex esse provident.', ' Quidem temporibus libero ipsa sapiente iusto iusto harum aut itaque incidunt.', 'Libero ipsa sapiente porro iusto harum aut itaque incidunt impedit iusto harum aut recusandae rem?', 'Repellat quae, sed non saepe aliquam nisi voluptate recusandae rem reiciendis iusto ducimus repudiandae facere.']
 
 const phrase = document.querySelector('#changing-phrase').textContent
 
